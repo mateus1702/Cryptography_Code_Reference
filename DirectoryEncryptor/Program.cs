899c718ce07dc6f7fe71c73b6077e2175767590b
mateus ribeiro de oliveira <mateus@mateusribeirooliveira.com>
2018-05-28 17:45:39 -0300

    * from password typing

--- DirectoryEncryptor/Program.cs
+++ DirectoryEncryptor/Program.cs
@@ -29,7 +29,7 @@ namespace DirectoryEncryptor
             Console.WriteLine("Type in the folder name:");
             FolderName = Console.ReadLine();
             Console.WriteLine("Type in the password:");
-            Password = Console.ReadLine();
+            Password = ReadPassword('*');
 
             var folder = @"C:\Users\Mateus\Documents\" + FolderName;
 
@@ -72,6 +72,45 @@ namespace DirectoryEncryptor
             ZipFile.ExtractToDirectory(zipFilePath, folderPath);
 
             File.Delete(zipFilePath);
+        }
+
+        public static string ReadPassword(char mask)
+        {
+            const int ENTER = 13, BACKSP = 8, CTRLBACKSP = 127;
+            int[] FILTERED = { 0, 27, 9, 10 /*, 32 space, if you care */ }; // const
+
+            var pass = new Stack<char>();
+            char chr = (char)0;
+
+            while ((chr = System.Console.ReadKey(true).KeyChar) != ENTER)
+            {
+                if (chr == BACKSP)
+                {
+                    if (pass.Count > 0)
+                    {
+                        System.Console.Write("\b \b");
+                        pass.Pop();
+                    }
+                }
+                else if (chr == CTRLBACKSP)
+                {
+                    while (pass.Count > 0)
+                    {
+                        System.Console.Write("\b \b");
+                        pass.Pop();
+                    }
+                }
+                else if (FILTERED.Count(x => chr == x) > 0) { }
+                else
+                {
+                    pass.Push((char)chr);
+                    System.Console.Write(mask);
+                }
+            }
+
+            System.Console.WriteLine();
+
+            return new string(pass.Reverse().ToArray());
         }
     }
 
